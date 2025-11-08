664d82fe77598957a4229dc5e2495b362eab2a33
Edwin Tandiono <tandiono98@gmail.com>
2025-11-08 14:45:19 +0700

    feat(deployment): Test env

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,3 +1,9 @@
 export function Welcome() {
-  return <h1>FINANCE YOURSELF</h1>
+  console.log('===', import.meta.env)
+  return (
+    <div>
+      <h1>FINANCE YOURSELF</h1>
+      <span>{`Test env:"${import.meta.env.VITE_TEST_ENV}"`}</span>
+    </div>
+  );
 }
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  basename: '/finance-yourself',
+  basename: '/finance-yourself/',
   ssr: false,
 } satisfies Config;
